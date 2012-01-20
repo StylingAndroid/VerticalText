a6523e2e76d21fde3a590364006bde34008dbf9b
Mark Allison <mark@stylingandroid.com>
2012-01-20 07:50:26 +0000

    Added code for Part 2

--- src/com/stylingandroid/verticaltext/VerticalTextActivity.java
+++ src/com/stylingandroid/verticaltext/VerticalTextActivity.java
@@ -2,6 +2,8 @@ package com.stylingandroid.verticaltext;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class VerticalTextActivity extends Activity
 {
@@ -11,5 +13,9 @@ public class VerticalTextActivity extends Activity
 	{
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.main );
+
+		TextView tv = (TextView) findViewById( R.id.animated_rotation );
+		tv.startAnimation( AnimationUtils.loadAnimation( this,
+				R.anim.rotatetext ) );
 	}
 }
